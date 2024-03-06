1837eea6ac476a4af052622f1a544850b7001103
IAmCellist <estuardogonzalezjr@gmail.com>
2024-03-05 19:11:03 -0500

    Fixed producer error, forgot to replace strings

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -12,13 +12,13 @@ class mqProducer(mqProducerInterface):
     self.con_params = pika.URLParameters(os.environ["AMQP_URL"])
     self.connection = pika.BlockingConnection(parameters=self.con_params)
     self.channel = self.connection.channel()
-    self.exchange = self.channel.exchange_declare(exchange="Exchange Name")
+    self.exchange = self.channel.exchange_declare(exchange=self.exchange_name)
 
   def publishOrder(self, message: str):
     self.channel.basic_publish(
-    exchange="Exchange Name",
-    routing_key="Routing Key",
-    body="Message",)
+    exchange=self.exchange_name,
+    routing_key=self.routing_key,
+    body=message,)
 
     self.channel.close()
     self.connection.close()
